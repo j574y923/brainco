dc47006299fd6da164aaa1a7dfe56026d9611dd2
Jesse Y <j574y923@gmail.com>
2019-06-22 23:10:45 -0500

    add /users/query and /groups/query

--- src/main/java/com/example/demo/BrainCorpController.java
+++ src/main/java/com/example/demo/BrainCorpController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.json.*;
@@ -29,7 +30,56 @@ public class BrainCorpController {
 			@RequestParam(required = false) String comment,
 			@RequestParam(required = false) String home,
 			@RequestParam(required = false) String shell) {
-		return "employees request by dept: " + name + " " + uid + " " + gid;
+		ConfigReader configReader = new ConfigReader();
+		configReader.read();
+		PasswdReader passwdReader = new PasswdReader(configReader.getPasswdPath());
+		passwdReader.read();
+
+		JsonArrayBuilder jsonArrBuilder = Json.createArrayBuilder();
+		JsonArray jsonArr = passwdReader.getContentsJson();
+		for(JsonValue jsonObj : jsonArr) {
+			if (name != null) {
+				JsonString jsonName = (JsonString) ((JsonObject) jsonObj).get("name");
+				String jsonNameTidy = jsonName.toString();
+				jsonNameTidy = jsonNameTidy.substring(1, jsonNameTidy.length() - 1);
+				if (!jsonNameTidy.equals(name))
+					continue;
+			}
+			if (uid != null) {
+				JsonNumber jsonUid = (JsonNumber) ((JsonObject)jsonObj).get("uid");
+				if (!jsonUid.toString().equals(uid))
+					continue;
+			}
+			if (gid != null) {
+				JsonNumber jsonGid = (JsonNumber) ((JsonObject)jsonObj).get("gid");
+				if (!jsonGid.toString().equals(gid))
+					continue;
+			}
+			if (comment != null) {
+				JsonString jsonComment = (JsonString) ((JsonObject)jsonObj).get("comment");
+				String jsonCommentTidy = jsonComment.toString();
+				jsonCommentTidy = jsonCommentTidy.substring(1, jsonCommentTidy.length() - 1);
+				if (!jsonCommentTidy.equals(comment))
+					continue;
+			}
+			if (home != null) {
+				JsonString jsonHome = (JsonString) ((JsonObject)jsonObj).get("home");
+				String jsonHomeTidy = jsonHome.toString();
+				jsonHomeTidy = jsonHomeTidy.substring(1, jsonHomeTidy.length() - 1);
+				if (!jsonHomeTidy.equals(home))
+					continue;
+			}
+			if (shell != null) {
+				JsonString jsonShell = (JsonString) ((JsonObject)jsonObj).get("shell");
+				String jsonShellTidy = jsonShell.toString();
+				jsonShellTidy = jsonShellTidy.substring(1, jsonShellTidy.length() - 1);
+				if (!jsonShellTidy.equals(shell))
+					continue;
+			}
+			
+			jsonArrBuilder.add(jsonObj);
+		}
+		return jsonArrBuilder.build().toString();
 	}
 
 	@RequestMapping(value = "users/{uid}")
@@ -49,12 +99,12 @@ public class BrainCorpController {
 
 	@RequestMapping(value = "users/{uid}/groups")
 	public @ResponseBody String getGroupsUid(@PathVariable("uid") String uid) {
-		//TODO: get all groups (make a jsonarray)
 		ConfigReader configReader = new ConfigReader();
 		configReader.read();
 		PasswdReader passwdReader = new PasswdReader(configReader.getPasswdPath());
 		passwdReader.read();
 		
+		// find username
 		JsonArray jsonArr = passwdReader.getContentsJson();
 		String username = "";
 		for(JsonValue jsonObj : jsonArr) {
@@ -65,19 +115,22 @@ public class BrainCorpController {
 			}
 		}
 		
+		// create json array with groups with username
 		GroupReader groupReader = new GroupReader(configReader.getGroupPath());
 		groupReader.read();
+		JsonArrayBuilder jsonArrBuilder = Json.createArrayBuilder();
 		if (!username.equals("")) {
 			jsonArr = groupReader.getContentsJson();
 			for(JsonValue jsonObj : jsonArr) {
 				JsonArray jsonMembers = (JsonArray) ((JsonObject)jsonObj).get("members");
 				for(JsonValue jsonMember : jsonMembers) {
-					if (jsonMember.toString().equals(username))
-						return jsonObj.toString();
+					if (jsonMember.toString().equals(username)) {
+						jsonArrBuilder.add(jsonObj);
+					}
 				}
 			}
 		}
-		return "";
+		return jsonArrBuilder.build().toString();
     }
 
 	@RequestMapping(value = "/groups")
@@ -93,7 +146,39 @@ public class BrainCorpController {
 	public @ResponseBody String getGroupsQuery(@RequestParam(required = false) String name,
 			@RequestParam(required = false) String gid,
 			@RequestParam(required = false) List<String> member) {
-		return "TODO" + member;
+		ConfigReader configReader = new ConfigReader();
+		configReader.read();
+		GroupReader groupReader = new GroupReader(configReader.getGroupPath());
+		groupReader.read();
+
+		JsonArrayBuilder jsonArrBuilder = Json.createArrayBuilder();
+		JsonArray jsonArr = groupReader.getContentsJson();
+		for(JsonValue jsonObj : jsonArr) {
+			if (name != null) {
+				JsonString jsonName = (JsonString) ((JsonObject) jsonObj).get("name");
+				String jsonNameTidy = jsonName.toString();
+				jsonNameTidy = jsonNameTidy.substring(1, jsonNameTidy.length() - 1);
+				if (!jsonNameTidy.equals(name))
+					continue;
+			}
+			if (gid != null) {
+				JsonNumber jsonGid = (JsonNumber) ((JsonObject)jsonObj).get("gid");
+				if (!jsonGid.toString().equals(gid))
+					continue;
+			}
+			if (member != null) {
+				JsonArray jsonMembers = (JsonArray) ((JsonObject)jsonObj).get("members");
+				List<String> strMembers = new ArrayList<String>();
+				for (int i = 0; i < jsonMembers.size(); i++) {
+					strMembers.add(jsonMembers.getString(i));
+				}
+				if (!strMembers.containsAll(member))
+					continue;
+			}
+			
+			jsonArrBuilder.add(jsonObj);
+		}
+		return jsonArrBuilder.build().toString();
     }
 	
 	@RequestMapping(value = "groups/{gid}")
